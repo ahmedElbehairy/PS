b1f11cdf8ab98b7dcd999e87970b23ee343b3604
ahaym2001 <ahaym2001@gmail.com>
2025-06-13 15:58:16 +0000

    home page

---
+++ src/app/playstation-landing/playstation-landing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playstation-landing',
+  imports: [],
+  templateUrl: './playstation-landing.html',
+  styleUrl: './playstation-landing.css'
+})
+export class PlaystationLanding {
+
+}
